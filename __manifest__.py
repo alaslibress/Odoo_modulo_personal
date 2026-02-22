d6162da5ea59b1f10262d50d46357e5e16db4bc1
alaslibres <jandroman23@gmail.com>
2026-02-22 13:09:31 +0100

    subido modulo personal

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "modulo-alejandro",
+    'summary': "El primer modulo de la clase de SGE",
+    'description': "Este modulo ha sido creado con scaffolding y esta sincronizado en mi repositorio Github",
+    'author': "Alejandro Pozo",
+    'website': "https://www.ieshlanz.es",
+    'category': 'Uncategorized',
+    'version': '19.0.0.1',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+}
